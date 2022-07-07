fee8376c7337931ba0f9e6147b5aab605132ad6f
pakon <luapk1996@hotmail.com>
2022-07-07 11:28:34 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,174 @@
+#include <torch/torch.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>       /* sin */
+#include <random>
+#include "Net.h"
+
+// Device
+auto cuda_available = torch::cuda::is_available();
+torch::Device device(cuda_available ? torch::kCUDA : torch::kCPU);
+std::default_random_engine generator;
+
+class CustomEnv{
+public:
+    int min_action;
+    int max_action;
+    int start;
+    int goal;
+    int low_state;
+    int high_state;
+    int num_steps;
+    float state;
+
+    CustomEnv(){
+        this->min_action = -5;
+        this->max_action = 5;
+        this->start = 0;
+        this->goal = 100;
+        this->low_state = -150;
+        this->high_state = 150;
+        this->num_steps = 0;
+        this->state = 0.0;
+    }
+
+    std::tuple<float, float, bool> step(float action){
+        bool done = 0;
+        this->num_steps++;
+        this->state += action;
+
+        float reward = action - 6;
+
+        if (state >= goal){
+            reward = 20;
+            done = true;
+        }
+
+        if (num_steps == 30){
+            done = true;
+        }
+
+        return {state, reward, done};
+    }
+
+    float reset(){
+        this->num_steps = 0;
+        this->state = 0.0;
+
+        return this->state;
+    }
+};
+
+class Agent{
+public:
+    float gamma = 0.99;
+    int n_outputs = 1;
+    int n_actions = 2;
+    NeuralNet actor = nullptr;
+    NeuralNet critic = nullptr;
+    int layer1_size = 64;
+    int layer2_size = 64;
+    torch::Tensor log_probs;
+    torch::optim::Adam *actor_optimizer;
+    torch::optim::Adam *critic_optimizer;
+
+    Agent(float alpha, float beta, int input_dims){
+        this->actor = NeuralNet(input_dims, layer1_size, n_actions);
+        this->actor->to(device);
+
+       critic = NeuralNet(input_dims, layer1_size, 1);
+       critic->to(device);
+
+        // Optimizer
+        actor_optimizer = new torch::optim::Adam(actor->parameters(), torch::optim::AdamOptions(alpha));
+        critic_optimizer = new torch::optim::Adam(critic->parameters(), torch::optim::AdamOptions(beta));
+    }
+
+    float choose_action(float observation){
+        torch::Tensor logsigma;
+        torch::Tensor output = actor->forward(torch::tensor(observation));
+        torch::Tensor mu = output[0];
+        logsigma = output[1]; //add exp of sigma
+        torch::Tensor sigma = torch::exp(logsigma);
+
+        std::normal_distribution<float> distribution(mu.item<float>(), sigma.item<float>());
+
+        // auto sampler1 = torch::randn({1}) * sigma + mu ;
+        // auto pdf = (1.0 / (sigma * std::sqrt(2.0 * M_PI))) * torch::exp(-0.5 * torch::pow((sampler1 - mu) / sigma, 2));
+        // this->log_probs = torch::log(pdf);
+
+        // float action = tanh(sampler1.item<float>());
+
+        auto probs = distribution(generator);
+
+        this->log_probs = torch::log(torch::tensor(probs));
+
+        float action = tanh(probs);
+
+        return action * 5;
+    }
+
+    void learn(float state, float reward, float new_state, bool done){
+        this->actor_optimizer->zero_grad();
+        this->critic_optimizer->zero_grad();
+
+        torch::Tensor critic_value_ = this->critic->forward(torch::tensor(new_state));
+        torch::Tensor critic_value = this->critic->forward(torch::tensor(state));
+
+        torch::Tensor tensor_reward = torch::tensor(reward);
+        torch::Tensor delta = tensor_reward + this->gamma*critic_value_ * (1 * int(done)) - critic_value;
+
+        auto actor_loss = -1 * this->log_probs * delta;
+        auto critic_loss = pow(delta, 2);
+
+        (actor_loss + critic_loss).backward();
+        this->actor_optimizer->step();
+        this->critic_optimizer->step();
+    }
+};
+
+int main() {
+    std::cout << "FeedForward Neural Network\n\n";
+
+    // Hyper parameters
+    const int64_t input_size = 1;
+    const int64_t hidden_size = 256;
+    const int64_t num_classes = 2;
+    const int64_t batch_size = 100;
+    const size_t num_epochs = 5;
+    const double alpha = 0.000005;
+    const double beta = 0.00001;
+
+    CustomEnv env;
+
+    Agent *agent = new Agent(0.000005, 0.00001, 1);
+
+    int num_episodes = 1500;
+    bool done;
+    float score;
+    float observation, observation_, reward, action;
+
+    // Set floating point output precision
+    std::cout << std::fixed << std::setprecision(4);
+
+    std::cout << "Training...\n";
+
+    // Train the model
+    for (int i = 0; i < num_episodes; i++){
+        done = false;
+        score = 0;
+        observation = env.reset();
+
+        while (!done){
+            action = agent->choose_action(observation);
+            std::tie(observation_, reward, done) = env.step(action);
+            agent->learn(observation, reward, observation_, done);
+            observation = observation_;
+            score += reward;
+        }
+
+        printf("Episode %d score %.2f\n", i, score);
+    }
+
+    return 0;
+}
